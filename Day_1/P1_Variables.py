8ad8145bc0e31452b49be11bd6613c7815229dbe
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-16 14:08:37 +0530

    Remove old Python scripts and add Hello World example in a new file

--- Day_1/P1_Variables.py
+++
@@ -1,7 +0,0 @@
-age = 21
-name = "yash"
-is_student = True
-
-print(age)
-print(name)
-print(is_student)
--- Day_1/P2_KeyWords_List.py
+++
@@ -1,4 +0,0 @@
-# Python Keywords List
-# This script prints the list of all Python keywords.
-import keyword
-print(keyword.kwlist)
