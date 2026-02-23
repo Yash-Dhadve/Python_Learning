14ef1e6788756c729f689ff380c034c8a5c78646
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-23 14:13:17 +0530

    Add Person class to demonstrate object creation and attribute usage

---
+++ Day-1/Practice-Set/Person.py
@@ -0,0 +1,13 @@
+# Create a Person class and print age.
+
+class person:
+    def __init__(self):
+        name = ""
+        age = 0
+
+p1 = person()
+p1.name = "Yash"
+p1.age = 21
+
+print(f"Name: {p1.name}")
+print(f"Age: {p1.age}")
