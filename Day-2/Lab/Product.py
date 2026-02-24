a93768dcdf06c05b1af729cd2fbace8cecfb5f68
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-24 10:34:26 +0530

    Fix class names to follow proper capitalization for Laptop, Movie, and Student classes

--- Day-2/Lab/Product.py
+++ Day-2/Lab/Product.py
@@ -10,21 +10,24 @@ Then create 2 objects and print total cost:
 total = price * quantity
 '''
 
-class product:
+class Product:
     def __init__(self, productName, price, quantity):
         self.productName = productName
         self.price = price
         self.quantity = quantity
-        self.total = price*quantity
 
-p1 = product("ABC", 20, 5)
-print(f"Laptop Brand: {p1.productName}")
+    def get_total(self):
+        return self.price * self.quantity
+    
+
+p1 = Product("ABC", 20, 5)
+print(f"Product Name: {p1.productName}")
 print(f"Price: {p1.price}")
 print(f"Quantity: {p1.quantity}")
-print(f"Total: {p1.total}\n")
+print(f"Total: {p1.get_total()}\n")
 
-p2 = product("XYZ", 10, 4)
-print(f"Laptop Brand: {p2.productName}")
-print(f"Price: {p2.price}")
+p2 = Product("XYZ", 10, 4)
+print(f"Product Name: {p2.productName}")
+print(f"Price: {p2.price}") 
 print(f"Quantity: {p2.quantity}")
-print(f"Total: {p2.total}")
+print(f"Total: {p2.get_total()}")
