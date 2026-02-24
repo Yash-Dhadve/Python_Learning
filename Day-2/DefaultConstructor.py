9297c7bd107c0e38a67dad49a16d556d0052f321
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-24 09:18:42 +0530

    Add Student class to demonstrate default constructor and attribute initialization

---
+++ Day-2/DefaultConstructor.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self):
+        self.name = "Unknown"
+        self.marks = 0
+
+s0 = Student()
+print(f"Default Name: {s0.name}")
+print(f"Default Marks: {s0.marks}")
