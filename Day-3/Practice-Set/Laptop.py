7b61ebc60494209f2fc81fa42717bb20dbd70ad9
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-25 12:30:19 +0530

    Add Laptop class with class variable for brand

---
+++ Day-3/Practice-Set/Laptop.py
@@ -0,0 +1,11 @@
+# Create a Laptop class with class variable brand = "Dell".
+
+class Laptop:
+    brand = "Dell"
+
+
+lap1 = Laptop();
+lap2 = Laptop();
+
+print(f"{lap1.brand} {lap2.brand}")
+        
