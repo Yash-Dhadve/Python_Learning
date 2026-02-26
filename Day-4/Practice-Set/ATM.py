7e2fb18927b00f0cc8093cf28ad7217d138a90f9
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-26 14:06:27 +0530

    feat: create ATM.py

---
+++ Day-4/Practice-Set/ATM.py
@@ -0,0 +1,21 @@
+# Create class ATM with method withdraw().
+
+class ATM:
+
+    def __init__(self, accountHolder):
+        self.accountHolder = accountHolder
+        self.balance = 1000
+
+    def withdraw(self,amount):
+        if (self.balance>=amount):
+            self.balance -= amount
+            print("Amount Withdrawal Successful\n")
+        else:
+            print("Not Sufficient Balance!\n") 
+
+acc1 = ATM("Yash")
+
+print(f"Account Holder Name: {acc1.accountHolder}\nBalance: {acc1.balance}\n")
+
+acc1.withdraw(100)
+print(f"Account Holder Name: {acc1.accountHolder}\nBalance: {acc1.balance}")
