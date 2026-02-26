2cc5fd561beeaa7dd62d44866ccc38c6bbc3a2a4
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-02-26 13:36:08 +0530

    feat: create Circle.py

---
+++ Day-4/Practice-Set/Circle.py
@@ -0,0 +1,10 @@
+# Create class Circle with method area()
+
+class Circle:
+
+    def area(self,r):
+        return 3.14*(r*r)
+
+c1 = Circle()
+result = c1.area(2)
+print(f"Area of Circle: {result}")
