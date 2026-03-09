5e60b1d167dc2207c01b3941b5eb33cd93130dbd
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-03-09 09:11:02 +0530

    Add method display_details() and override in child.

--- Day-10/Miniproject/main.py
+++ Day-10/Miniproject/main.py
@@ -15,6 +15,10 @@ class BankAccount:
     def getBalance(self):
         return self.__balance
 
+    def display_details(self):
+        print("Account Holder: ",self.accountHolder);
+        print("Account Balance: ",self.__balance);
+
 
 class SavingsAccount(BankAccount):
 
@@ -25,10 +29,17 @@ class SavingsAccount(BankAccount):
     def calculateInterest(self):
         return self.getBalance() * (self.__interestRate/100)
     
+    def display_details(self):
+        super().display_details()
+        print("Account Interest Rate: ",self.__interestRate);
+        print("Calculated Interest: ",self.calculateInterest(),"\n");
+    
 acc1 = SavingsAccount("Yash Dhadve", 1000, 2)
 
-print("Account Balance: ",acc1.getBalance(),"\n");
+print("Account Balance: ",acc1.getBalance());
 acc1.deposit(500);
-print("Account Balance After Depositing: ",acc1.getBalance(),"\n");
+print("Account Balance After Depositing: ",acc1.getBalance());
 acc1.withdraw(200);
 print("Account Balance After Withdraw: ",acc1.getBalance(),"\n");
+
+acc1.display_details()
